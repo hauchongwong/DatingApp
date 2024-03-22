99f0fbd39c6e39a5395a19e2520c0179448282d9
hauchongwong <hauchongwong@huttonsgroup.com>
2024-03-22 20:27:53 +0800

    end of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { User } from '../_models/user';
 import { Observable, of } from 'rxjs';
+import { Route, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -14,7 +16,8 @@ export class NavComponent implements OnInit {
 //currentUsers$ : Observable<User | null> = of(null);
 
 
-  constructor(public accountService:AccountService) {}
+  constructor(public accountService:AccountService,private router: Router,
+    private toastr: ToastrService) {}
 
   ngOnInit(): void {    
     //this.getCurrentUser();
@@ -28,20 +31,27 @@ export class NavComponent implements OnInit {
   //   })
   // }
 
+  // login() {
+  //   this.accountService.login(this.model).subscribe({
+  //     next: response => {
+  //      // console.log(response);
+  //      // this.loggedIn = true;
+  //      this.router.navigateByUrl('/members')
+  //     },
+  //     error: error=>console.log(error)
+  //   })
+  // }
+
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-       // console.log(response);
-       // this.loggedIn = true;
-      },
-      error: error=>console.log(error)
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error=>this.toastr.error(error.error)
     })
-
-
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
    // this.loggedIn = false;
   }
 }
